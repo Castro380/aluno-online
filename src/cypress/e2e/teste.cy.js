d9bf47169d41a1115eca2927a9c12f4bbebefe18
Castro380 <mateuso@iesb.edu.br>
2023-06-14 21:26:50 -0300

    trabalho software

--- src/cypress/e2e/teste.cy.js
+++ src/cypress/e2e/teste.cy.js
@@ -1,9 +1,6 @@
 describe('Testando',()=>{
     it('Cypress',()=>{
-       cy.visit('/')
-       cy.get('#inputMatricula').type('2023100')
-       cy.get('#inputSenha').type('senha123')
-       cy.get('body > form > button').click()
+       cy.visit('/requerimentos')
        cy.get('#navbarToggler1 > ul.navbar-nav.mr-auto.mt-2.mt-lg-0 > li:nth-child(5) > a').click()
        cy.get('body > div > div > div.card-header > a').click()
        cy.get("#inputDisciplina").select("ADS032 - Construção de Frontend")
